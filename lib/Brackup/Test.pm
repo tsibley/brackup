a0c95a02bf9cea511e834eb1c89ac95e42de897c
Thomas Sibley <tsibley@cpan.org>
2014-11-08 16:03:27 -0800

    Ensure Data::Dumper hash output is sorted so tests pass on 5.18+

--- lib/Brackup/Test.pm
+++ lib/Brackup/Test.pm
@@ -175,6 +175,7 @@ sub ok_dirs_match {
 
     if ($has_diff) {
         use Data::Dumper;
+        local $Data::Dumper::Sortkeys = 1;
         my $pre_dump = Dumper($pre_ls);
         my $post_dump = Dumper($post_ls);
         my $diff = Text::Diff::diff(\$pre_dump, \$post_dump);
@@ -192,6 +193,7 @@ sub ok_files_match {
 
     if ($has_diff) {
         use Data::Dumper;
+        local $Data::Dumper::Sortkeys = 1;
         my $pre_dump = Dumper($pre_ls);
         my $post_dump = Dumper($post_ls);
         my $diff = Text::Diff::diff(\$pre_dump, \$post_dump);
